2d4eaf4185f1caf6d1cdffbf7cdbc5a25ba1c934
Arthur Barbosa <arthur.barbosa@itbam.org.br>
2020-09-24 16:31:04 -0400

    [added] criaçao do service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Pais } from './models/pais.model';
+import { Mundo } from './models/mundo.model';
+import { Component, OnInit } from '@angular/core';
+import { CovidapiService } from './services/covidapi.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-covid';
+export class AppComponent implements OnInit{
+  mundo: Mundo;
+  pais: Pais;
+
+  constructor(private apiService: CovidapiService){
+
+  }
+  
+  async ngOnInit() {
+    this.mundo = await this.apiService.getInfoMundo();
+    this.pais = await this.apiService.getInfoPais();
+  }
 }
